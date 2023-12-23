e5e8cca6854b1fb2a7710b64b46132001bb4d5b5
omegacms <omegacms@outlook.com>
2023-12-23 13:10:23 +0100

    Update Site Url

--- src/Adapter/AbstractQueueAdapter.php
+++ src/Adapter/AbstractQueueAdapter.php
@@ -30,9 +30,9 @@ use Omega\Queue\Job;
  * @category    Omega
  * @package     Omega\Queue
  * @subpackege  Omega\Queue\Adapter
- * @link        https://omegacms.github.com
+ * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.com)
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
--- src/Job.php
+++ src/Job.php
@@ -30,9 +30,9 @@ use Omega\Database\Model\AbstractModel;
  *
  * @category    Omega
  * @package     Omega\Queue
- * @link        https://omegacms.github.com
+ * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.com)
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
