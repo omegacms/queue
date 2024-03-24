a28c26e51d2d530485bb32f898ca13ba56deef95
omegacms <omegacms@outlook.com>
2024-03-24 11:42:00 +0100

    Fix namespece for new model package

--- src/Job.php
+++ src/Job.php
@@ -23,7 +23,7 @@ namespace Omega\Queue;
  */
 use function Omega\Helpers\config;
 use function unserialize;
-use Omega\Database\Model\AbstractModel;
+use Omega\Model\AbstractModel;
 
 /**
  * Job class.
